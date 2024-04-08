84885fd1961c3e047026abec9ace209f38bcbc11
Jarek Armatys <jarekarmatys@gmail.com>
2024-04-08 13:07:53 +0200

    39: Added MudBlazor UI package for blazor client

--- API/ASSISTENTE.Client/Program.cs
+++ API/ASSISTENTE.Client/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using ASSISTENTE.Client;
 using ASSISTENTE.Client.Common.Extensions;
+using MudBlazor.Services;
 
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -12,6 +13,8 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 var settings = builder.Configuration.GetSettings();
 
 builder.Services.AddSingleton(settings);
+
+builder.Services.AddMudServices();
 
 builder.Services.AddScoped(_ => new HttpClient { BaseAddress = new Uri(settings.ApiUrl) });
 
