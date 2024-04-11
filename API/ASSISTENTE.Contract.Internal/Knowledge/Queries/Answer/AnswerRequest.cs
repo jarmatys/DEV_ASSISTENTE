7cea4299c652b663003f1db3bdad268558b6b2d3
Jarek Armatys <jarekarmatys@gmail.com>
2024-04-11 14:08:34 +0200

    42: Created form for asking purpose

--- API/ASSISTENTE.Contract.Internal/Knowledge/Queries/Answer/AnswerRequest.cs
+++ API/ASSISTENTE.Contract.Internal/Knowledge/Queries/Answer/AnswerRequest.cs
@@ -1,3 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace ASSISTENTE.Contract.Internal.Knowledge.Queries.Answer;
 
-public sealed record AnswerRequest(string Question);
+public sealed class AnswerRequest
+{
+    [Required]
+    [StringLength(8, ErrorMessage = "Name length can't be more than 8.")]
+    public string Question { get; set; } = null!;
+};
