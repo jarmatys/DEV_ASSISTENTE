1e6e89f718deed3554fc1714077c040afc9eb677
Jarek Armatys <jarekarmatys@gmail.com>
2024-04-15 16:33:38 +0200

    46: Added SeqSection and cleanup in csproj files

--- API/ASSISTENTE.Client/Common/Settings/ClientSettings.cs
+++ API/ASSISTENTE.Client/Common/Settings/ClientSettings.cs
@@ -3,4 +3,5 @@ namespace ASSISTENTE.Client.Common.Settings;
 public sealed class ClientSettings
 {
     public required string ApiUrl { get; init; }
+    public required SeqSection Seq { get; init; }
 }
---
+++ API/ASSISTENTE.Client/Common/Settings/SeqSection.cs
@@ -0,0 +1,7 @@
+namespace ASSISTENTE.Client.Common.Settings;
+
+public sealed class SeqSection
+{
+    public required string Url { get; init; }
+    public required string ApiKey { get; init; }
+}
